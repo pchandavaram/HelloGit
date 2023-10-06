2d6d8da4b2401d644cadac2d9136d8f07d2f528c
pavan <pcchandavaram@gmail.comgit config --global user.email pchandavaram@gmail.com>
2023-10-06 10:19:42 -0400

    added line

--- src/pc/Testing2.java
+++ src/pc/Testing2.java
@@ -4,7 +4,9 @@ public class Testing2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("Hi");
+   System.out.println("Hi pavan");
+   System.out.println("How r ya?");
+
 	}
 
 }
